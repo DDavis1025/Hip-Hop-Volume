cd6581f8909395b7629841ae96bb27305ba9da0f
DDavis1025 <1dillondavis@gmail.com>
2020-09-15 15:50:43 -0400

    Initial Commit

---
+++ Hip-Hop Volume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hip-Hop Volume
+//
+//  Created by Dillon Davis on 9/15/20.
+//  Copyright © 2020 Dillon Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
